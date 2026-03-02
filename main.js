d68a4df46b36dc6297f7f047a0950bbcef5211fb
maruwaruharu <maruwaruharu@gmail.com>
2026-03-02 14:06:10 +0000

    Add button functionality to display 'Hello, world!'

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// Add JS here
+document.getElementById('add-hello-btn').addEventListener('click', () => {
+  const container = document.getElementById('hello-container');
+  const newHello = document.createElement('p');
+  newHello.textContent = 'Hello, world!';
+  container.appendChild(newHello);
+});
